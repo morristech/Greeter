89b5e5305e4f8a9a9c03af78b7040ff21e56a26e
Joel Roman Sosa <joelrsosarivera@gmail.com>
2018-04-27 20:09:09 -0400

    Implement get started button. Begin setting bintray

--- greeter/src/main/java/com/jrsosadev/greeter/GreetActivity.java
+++ greeter/src/main/java/com/jrsosadev/greeter/GreetActivity.java
@@ -4,15 +4,16 @@ import android.os.Bundle;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 
 import com.rd.PageIndicatorView;
-import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 
-public class GreetActivity extends AppCompatActivity implements ViewPager.OnPageChangeListener {
+public class GreetActivity extends AppCompatActivity implements ViewPager.OnPageChangeListener, View.OnClickListener {
 
+    public static final int GREET_RESULT_COMPLETED = 1;
     public static final String GREET_BUNDLE_EXTRA = "greet.bundle.extra";
 
     Bundle args;
@@ -27,6 +28,7 @@ public class GreetActivity extends AppCompatActivity implements ViewPager.OnPage
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_greet);
         greetEndButton = findViewById(R.id.greet_end_button);
+        greetEndButton.setOnClickListener(this);
         pageIndicatorView = findViewById(R.id.page_indicator_view);
         args = getIntent().getExtras();
         greets = getIntent().getParcelableArrayListExtra(GREET_BUNDLE_EXTRA);
@@ -66,5 +68,11 @@ public class GreetActivity extends AppCompatActivity implements ViewPager.OnPage
     @Override
     public void onPageScrollStateChanged(int state) {
 
+    }
+
+    @Override
+    public void onClick(View v) {
+        finish();
+        setResult(GREET_RESULT_COMPLETED);
     }
 }
